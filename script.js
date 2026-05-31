9756ee05116a166ce5a11fc803fb676b19986ddd
Carlytcheu12 <tcheucarlane@gmail.com>
2026-05-31 16:08:07 +0100

    modify

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
-const btn = document.getElementById("modeToggle");
-btn.addEventListener("click", () => {
-  document.body.classList.toggle("light");
+const btn = document.getElementById('toggle');
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('light');
 });
+
+// typing animation
+const text = "Hi, I'm Maeva Tcheuffa";
+let i = 0;
+function type(){
+  if(i < text.length){
+    document.querySelector(".typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 100);
+  }
+}
+type();
